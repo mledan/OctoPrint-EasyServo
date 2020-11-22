bf5a9ee8c54d8c9f1a287b156b50889c4c91444e
Guyot François <51274081+iFrostizz@users.noreply.github.com>
2020-11-23 01:03:42 +0200

    v2.0.1

--- EasyServo/__init__.py
+++ EasyServo/__init__.py
@@ -4,8 +4,9 @@ import time
 
 import octoprint.plugin
 import pigpio
+import threading
 
-class EasyservoPlugin(octoprint.plugin.SettingsPlugin, 
+class EasyservoPlugin(octoprint.plugin.SettingsPlugin,
                       octoprint.plugin.AssetPlugin,
                       octoprint.plugin.TemplatePlugin,
                       octoprint.plugin.StartupPlugin,
@@ -17,14 +18,18 @@ class EasyservoPlugin(octoprint.plugin.SettingsPlugin,
 	##~~ SettingsPlugin mixin
 	def get_settings_defaults(self):
 		return dict(
-			GPIOX=12,
-			GPIOY=13,
-			xAutoAngle=90,
-			yAutoAngle=90,
-			xInvert=False,
-			yInvert=False,
-			saved_angle_x=90,
-			saved_angle_y=90
+			GPIOX="12",
+			GPIOY="13",
+			xAutoAngle="90",
+			yAutoAngle="90",
+			xInvert="False",
+			yInvert="False",
+			saved_angle_x="90",
+			saved_angle_y="90",
+			sleepTimeX="10",
+			sleepTimeY="10",
+			xAngle="90",
+			yAngle="90"
 		)
 
 	def on_settings_save(self, data):
@@ -38,14 +43,12 @@ class EasyservoPlugin(octoprint.plugin.SettingsPlugin,
 
 		if oldGPIOX != newGPIOX:
 			self._logger.info("GPIO x changed, initializing.")
-			self.pi.set_servo_pulsewidth(newGPIOX, 0)
-			self._logger.info("moving GPIO %d to %d degrees" % (newGPIOX, self.current_angle_x))
-			self.current_angle_x = self.move_servo_by(GPIOX, self.current_angle_x)
+			xAutoAngle = self._settings.get_int(["xAutoAngle"])
+			self.pi.set_servo_pulsewidth(newGPIOX, xAutoAngle)
 		if oldGPIOY != newGPIOY:
 			self._logger.info("GPIO y changed, initiliazing.")
-			self.pi.set_servo_pulsewidth(newGPIOY, 0)
-			self._logger.info("moving GPIO %d to %d degrees" % (newGPIOY, self.current_angle_y))
-			self.current_angle_y = self.move_servo_by(GPIOY, self.current_angle_y)
+			yAutoAngle = self._settings.get_int(["yAutoAngle"])
+			self.pi.set_servo_pulsewidth(newGPIOY, yAutoAngle)
 
 
 	##~~ AssetPlugin mixin
@@ -60,46 +63,20 @@ class EasyservoPlugin(octoprint.plugin.SettingsPlugin,
 	##~~ StartupPlugin mixin
 
 	def on_after_startup(self):
-		if self.pi == None:
-			self._logger.info("Initiliazing pigpio")
+		if self.pi is None:
+			self._logger.info("Initializing pigpio")
 			self.pi = pigpio.pi()
 			self._logger.info(self.pi)
 		if not self.pi.connected:
-			self._logger.info("There was an error initiliazing pigpio")
+			self._logger.info("There was an error initializing pigpio")
 			return
 
 		GPIOX = self._settings.get_int(["GPIOX"])
 		GPIOY = self._settings.get_int(["GPIOY"])
 		xAutoAngle = self._settings.get_int(["xAutoAngle"])
 		yAutoAngle = self._settings.get_int(["yAutoAngle"])
-		saved_angle_x = xAutoAngle
-		saved_angle_y = yAutoAngle
-		#self._logger.info("xAutoAngle {} yAutoAngle {}.".format(xAutoAngle, yAutoAngle))
-
-		
-		# initialize x axis
-		saved_angle_x = self._settings.get_int(["saved_angle_x"])
-		self.pi.set_servo_pulsewidth(GPIOX, 0)
-		if self._settings.get_boolean(["xInvert"])==True:
-			self._settings.set(["xInvert"], False)
-			self._settings.save()
-			self.current_angle_x = self.move_servo_to_ang(GPIOX, saved_angle_x, xAutoAngle)
-			self._settings.set(["xInvert"], True)
-			self._settings.save()
-		else:
-			self.current_angle_x = self.move_servo_to_ang(GPIOX, saved_angle_x, xAutoAngle)
-		# initialize y axis
-		saved_angle_y = self._settings.get_int(["saved_angle_y"])
-		self.pi.set_servo_pulsewidth(GPIOY, 0)
-		if self._settings.get_boolean(["yInvert"])==True:
-			self._settings.set(["yInvert"], False)
-			self._settings.save()
-			self.current_angle_y = self.move_servo_to_ang(GPIOY, saved_angle_y, yAutoAngle)
-			self._settings.set(["yInvert"], True)
-			self._settings.save()
-		else:
-			self.current_angle_y = self.move_servo_to_ang(GPIOY, saved_angle_y, yAutoAngle)
-
+		self.pi.set_servo_pulsewidth(GPIOX, self.angle_to_width(xAutoAngle))
+		self.pi.set_servo_pulsewidth(GPIOY, self.angle_to_width(yAutoAngle))
 
 	##~~ ShutdownPlugin mixin
 
@@ -107,7 +84,6 @@ class EasyservoPlugin(octoprint.plugin.SettingsPlugin,
 		if not self.pi.connected:
 			self._logger.info("There was an error on shutdown pigpio not connected")
 			return
-
 		GPIOX = self._settings.get_int(["GPIOX"])
 		GPIOY = self._settings.get_int(["GPIOY"])
 		self.pi.set_servo_pulsewidth(GPIOX, 0)
@@ -117,7 +93,8 @@ class EasyservoPlugin(octoprint.plugin.SettingsPlugin,
 	##-- Template hooks
 
 	def get_template_configs(self):
-		return [dict(type="settings",custom_bindings=False)]
+		return [dict(type="settings", custom_bindings=False),
+				dict(type="generic", template="EasyServo.jinja2", custom_bindings=True)]
 
 	##~~ Softwareupdate hook
 
@@ -141,83 +118,187 @@ class EasyservoPlugin(octoprint.plugin.SettingsPlugin,
 	##~~ Utility functions
 
 	def angle_to_width(self, ang): #Easier conversion for the angle
-		if ang > 180:
-			ang = 180
-		elif ang < 0:
-			ang = 0
-
-		ratio = (2500 - 500)/180 #Calcul ratio from angle to percent
-
+		ratio = (2500 - 500)/180
 		angle_as_width = ang * ratio
 		return int(500 + angle_as_width)
 
 	def width_to_angle(self, width):
-		ratio = (180.0)/(2500.0-500.0)
+		ratio = 180.0 / (2500.0 - 500.0)
+		width_as_angle = width * ratio
+		return int(width_as_angle - 45)
 
-		width_as_angle = float(width) * ratio
-		return float(width_as_angle - 45.0)
+	def move_servo_to_ang(self, pin, angle_to_reach): #Absolute positioning
+		sleepTime = 0
 
-	def move_servo_to_ang(self, pin, start_angle, destination_angle):
-		angle_difference = destination_angle - start_angle
-		move_results = self.move_servo_by(pin, angle_difference)
-		return move_results
+		if int(pin) == self._settings.get_int(["GPIOX"]):
+			sleepTime = self._settings.get_int(["sleepTimeX"])
+			if self._settings.get_boolean(["xInvert"]):
+				angle_to_reach = 180 - angle_to_reach
+		if int(pin) == self._settings.get_int(["GPIOY"]):
+			sleepTime = self._settings.get_int(["sleepTimeY"])
+			if self._settings.get_boolean(["yInvert"]):
+				angle_to_reach = 180 - angle_to_reach
 
-	def move_servo_by(self, pin, angle_difference):
-		if angle_difference < 0:
-			direction = -1
-		else:
-			direction = 1
+		actual_width = self.pi.get_servo_pulsewidth(int(pin))
+		actual_angle = self.width_to_angle(actual_width)
+		width_to_reach = self.angle_to_width(angle_to_reach)
 
-		if self._settings.get_int(["GPIOX"]) == int(pin):
-			angle_start = self._settings.get_int(["saved_angle_x"])
-			width_start = self.pi.get_servo_pulsewidth(int(pin))
-		if self._settings.get_int(["GPIOY"]) == int(pin):
-			angle_start = self._settings.get_int(["saved_angle_y"])
-			width_start = self.pi.get_servo_pulsewidth(int(pin))
+		self._logger.info("pin {} actual_width {} actual_angle {} width_to_reach {} angle_to_reach {}".format(pin, actual_width, actual_angle, width_to_reach, angle_to_reach))
 
-		angle_end = angle_start + angle_difference
-		width_end = self.angle_to_width(angle_end)
-		angle_current = angle_start
-		width_current = width_start
-		if self._settings.get_boolean(["xInvert"])==True and self._settings.get_int(["GPIOX"]) == int(pin):
-			direction = direction*-1
-			angle_end = -angle_end
-			angle_start = -angle_start
-		if self._settings.get_boolean(["yInvert"])==True and self._settings.get_int(["GPIOY"]) == int(pin):
-			direction = direction*-1
-			angle_end = -angle_end
-			angle_start = -angle_start
-		#self._logger.info("pin {} angle_start {} angle_end {} direction {} angle_difference {} width_start {} width_end {}.".format(pin, angle_start, angle_end, direction, angle_difference, width_start, width_end))
-		if width_start != width_end:
-			for x in range(0, int(((angle_end - angle_start) * direction) * 10)):
-				angle_current = angle_current + (0.1 * direction)
-				width_current = self.angle_to_width(angle_current)
-				self.pi.set_servo_pulsewidth(int(pin), width_current)
-				#self._logger.info("Moved GPIO {} to width: {} angle: {}.".format(pin, width_current, angle_current))
-				time.sleep(0.01)
-				if width_current > 2478 or width_current < 501: #Noticed that the angle was 180.0° for 2479us and 500 was giving strange values .......
-					break
-			if self._settings.get_int(["GPIOX"]) == int(pin):
-				self._settings.set(["saved_angle_x"], round((angle_current), 1))
-			if self._settings.get_int(["GPIOY"]) == int(pin):
-				self._settings.set(["saved_angle_y"], round((angle_current), 1))
-			self._settings.save()
-		return angle_current
+		if width_to_reach - actual_width >= 0:
+			incrementSign = 1
+		elif width_to_reach - actual_width < 0:
+			incrementSign = -1
 
-	##~~ atcommand hook
+		for x in range(actual_width, width_to_reach, incrementSign):
+			self.pi.set_servo_pulsewidth(int(pin), x)
+			self._logger.info("Setting the width of the pin {} at {} us".format(int(pin), x))
+			time.sleep(sleepTime / 1000)
+			width_current = self.pi.get_servo_pulsewidth(int(pin))
+			if width_current > 2477:  # Noticed that the angle was 180.0° for 2479us and 500 was giving strange values .......
+				self._logger.info("GPIO {} reached his boundaries with a {} pulse width".format(pin, width_current))
+				self.pi.set_servo_pulsewidth(int(pin), 2475)
+				break
+			elif width_current < 502:
+				self._logger.info("GPIO {} reached his boundaries with a {} pulse width".format(pin, width_current))
+				self.pi.set_servo_pulsewidth(int(pin), 504)
+				break
+
+	def move_servo_by(self, pin, angle_difference): #Relative positioning
+		direction = 1
+
+		actual_width = self.pi.get_servo_pulsewidth(int(pin))
+		actual_angle = self.width_to_angle(actual_width)
+		sleepTime = 0
+
+		if int(pin) == self._settings.get_int(["GPIOX"]):
+			sleepTime = self._settings.get_int(["sleepTimeX"])
+			if self._settings.get_boolean(["xInvert"]):
+				direction = -1
+			else:
+				direction = 1
+		if int(pin) == self._settings.get_int(["GPIOY"]):
+			sleepTime = self._settings.get_int(["sleepTimeY"])
+			if self._settings.get_boolean(["yInvert"]):
+				direction = -1
+			else:
+				direction = 1
+
+		angle_to_reach = actual_angle + angle_difference * direction
+		width_to_reach = self.angle_to_width(angle_to_reach)
+
+		self._logger.info("pin {} actual_width {} actual_angle {} angle_difference {} direction {} width_to_reach {} angle_to_reach {}".format(pin, actual_width, actual_angle, angle_difference, direction, width_to_reach, angle_to_reach))
+
+		if width_to_reach - actual_width >= 0:
+			incrementSign = 1
+		elif width_to_reach - actual_width < 0:
+			incrementSign = -1
+
+		for x in range(actual_width, width_to_reach, incrementSign):
+			self.pi.set_servo_pulsewidth(int(pin), x)
+			self._logger.info("Setting the width of the pin {} at {} us".format(int(pin), x))
+			time.sleep(sleepTime/1000)
+			width_current = self.pi.get_servo_pulsewidth(int(pin))
+			if width_current > 2477:  # Noticed that the angle was 180.0° for 2479us and 500 was giving strange values .......
+				self._logger.info("GPIO {} reached his boundaries with a {} pulse width".format(pin, width_current))
+				self.pi.set_servo_pulsewidth(int(pin), 2475)
+				break
+			elif width_current < 502:
+				self._logger.info("GPIO {} reached his boundaries with a {} pulse width".format(pin, width_current))
+				self.pi.set_servo_pulsewidth(int(pin), 504)
+				break
 
 	def processAtCommand(self, comm_instance, phase, command, parameters, tags=None, *args, **kwargs):
-		if command == 'EASYSERVO':
-			#get GPIO and angle from parameters
-			GPIO,ang= parameters.split(' ')
-			if int(GPIO) == self._settings.get_int(["GPIOX"]):
-				self.current_angle_x = self.move_servo_by(int(GPIO), int(ang))
-			elif int(GPIO) == self._settings.get_int(["GPIOY"]):
-				self.current_angle_y = self.move_servo_by(int(GPIO), int(ang))
+		self._logger.info("{} {}".format(command, parameters))
+		if command == 'EASYSERVO_REL':
+			GPIO, ang = parameters.split(' ')
+			if int(GPIO) == self._settings.get_int(["GPIOX"]) or int(GPIO) == self._settings.get_int(["GPIOY"]):
+				thread = threading.Thread(target=self.move_servo_by, args=(int(GPIO), int(ang)))
+				thread.daemon = True
+				thread.start()
+			else:
+				self._logger.info("unknown GPIO %d" % int(GPIO))
+		if command == 'EASYSERVO_ABS':
+			GPIO, ang = parameters.split(' ')
+			if int(GPIO) == self._settings.get_int(["GPIOX"]) or int(GPIO) == self._settings.get_int(["GPIOY"]):
+				thread = threading.Thread(target=self.move_servo_to_ang, args=(int(GPIO), int(ang)))
+				thread.daemon = True
+				thread.start()
 			else:
 				self._logger.info("unknown GPIO %d" % int(GPIO))
 		if command == 'EASYSERVOAUTOHOME':
-			self.on_after_startup()
+			if len(parameters.split(' ')) == 2:
+				GPIOX, GPIOY = parameters.split(' ')
+				self._logger.info("GPIOX {} GPIOY {}".format(GPIOX, GPIOY))
+				xAutoAngle = self._settings.get_int(["xAutoAngle"])
+				thread_x = threading.Thread(target=self.move_servo_to_ang, args=(int(GPIOX), xAutoAngle))
+				thread_x.daemon = True
+				thread_x.start()
+				yAutoAngle = self._settings.get_int(["yAutoAngle"])
+				thread_y = threading.Thread(target=self.move_servo_to_ang, args=(int(GPIOY), yAutoAngle))
+				thread_y.daemon = True
+				thread_y.start()
+			elif len(parameters.split(' ')) == 1:
+				GPIO = parameters
+				self._logger.info("GPIO {}".format(GPIO))
+				if int(GPIO) == self._settings.get_int(["GPIOX"]):
+					xAutoAngle = self._settings.get_int(["xAutoAngle"])
+					thread = threading.Thread(target=self.move_servo_to_ang, args=(int(GPIO), xAutoAngle))
+					thread.daemon = True
+					thread.start()
+				elif int(GPIO) == self._settings.get_int(["GPIOY"]):
+					yAutoAngle = self._settings.get_int(["yAutoAngle"])
+					thread = threading.Thread(target=self.move_servo_to_ang, args=(int(GPIO), yAutoAngle))
+					thread.daemon = True
+					thread.start()
+				else:
+					self._logger.info("unknown GPIO %d" % int(GPIO))
+
+
+	def process_gcode(self, comm, line, *args, **kwargs):
+		if line.startswith('EASYSERVO_REL'):
+			command, GPIO, ang = line.split()
+			if int(GPIO) == self._settings.get_int(["GPIOX"]) or int(GPIO) == self._settings.get_int(["GPIOY"]):
+				thread = threading.Thread(target=self.move_servo_by, args=(int(GPIO), int(ang)))
+				thread.daemon = True
+				thread.start()
+			else:
+				self._logger.info("unknown GPIO %d" % int(GPIO))
+		if line.startswith('EASYSERVO_ABS'):
+			command, GPIO, ang = line.split()
+			if int(GPIO) == self._settings.get_int(["GPIOX"]) or int(GPIO) == self._settings.get_int(["GPIOY"]):
+				thread = threading.Thread(target=self.move_servo_to_ang, args=(int(GPIO), int(ang)))
+				thread.daemon = True
+				thread.start()
+			else:
+				self._logger.info("unknown GPIO %d" % int(GPIO))
+		if line.startswith('EASYSERVOAUTOHOME'):
+			if len(line.split()) == 3:
+				command, GPIOX, GPIOY = line.split()
+				if int(GPIOX) == self._settings.get_int(["GPIOX"]) and int(GPIOY) == self._settings.get_int(["GPIOY"]):
+					xAutoAngle = self._settings.get_int(["xAutoAngle"])
+					thread_x = threading.Thread(target=self.move_servo_to_ang, args=(GPIOX, xAutoAngle))
+					thread_x.daemon = True
+					thread_x.start()
+					yAutoAngle = self._settings.get_int(["yAutoAngle"])
+					thread_y = threading.Thread(target=self.move_servo_to_ang, args=(GPIOY, yAutoAngle))
+					thread_y.daemon = True
+					thread_y.start()
+			elif len(line.split()) == 2:
+				command, GPIO = line.split()
+				if int(GPIO) == self._settings.get_int(["GPIOX"]):
+					xAutoAngle = self._settings.get_int(["xAutoAngle"])
+					thread = threading.Thread(target=self.move_servo_to_ang, args=(int(GPIO), xAutoAngle))
+					thread.daemon = True
+					thread.start()
+				elif int(GPIO) == self._settings.get_int(["GPIOY"]):
+					yAutoAngle = self._settings.get_int(["yAutoAngle"])
+					thread = threading.Thread(target=self.move_servo_to_ang, args=(int(GPIO), yAutoAngle))
+					thread.daemon = True
+					thread.start()
+				else:
+					self._logger.info("unknown GPIO %d" % int(GPIO))
+		return line
 
 
 __plugin_name__ = "Easy Servo"
@@ -230,5 +311,6 @@ def __plugin_load__():
 	global __plugin_hooks__
 	__plugin_hooks__ = {
 		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information,
-		"octoprint.comm.protocol.atcommand.queuing": __plugin_implementation__.processAtCommand
+		"octoprint.comm.protocol.atcommand.queuing": __plugin_implementation__.processAtCommand,
+		"octoprint.comm.protocol.gcode.received": __plugin_implementation__.process_gcode
 	}
